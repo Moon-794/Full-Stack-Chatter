9462c133733ad3ad6d4e874149d1dd0079564556
Moon-794 <21002961@hope.ac.uk>
2023-08-31 21:32:21 +0100

    Frontend Progress

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,10 +1,40 @@
+import "./App.css";
+
+function Message()
+{
+  return (
+  <div className="message">
+    <p className="message-p message-header">Username</p>
+    <p className="message-p message-content">Message Content</p>
+  </div>
+  );
+}
+
 function App() 
 {
   return (
-    <>
-      <p>Hello</p>
-    </>
+      <div className="main-container">
+        <div className="header">
+          <h1>Full Stack Chatter</h1>
+        </div>
+        <div className="message-list">
+          <Message />
+          <Message />
+          <Message />
+          <Message />
+          <Message />
+          <Message />
+          <Message />
+          <Message />
+          <Message />
+          <Message />
+        </div>
+        <div className="input-panel">
+
+        </div>
+      </div>
   )
 }
+
 
 export default App
