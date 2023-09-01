0bc0051559162ac277677d21524aebec0e7b44b0
Moon-794 <21002961@hope.ac.uk>
2023-09-01 12:49:16 +0100

    Added an input field

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,10 +1,11 @@
+import sendimg from '../public/send.png'
 import "./App.css";
 
 function Message()
 {
   return (
   <div className="message">
-    <p className="message-p message-header">Username</p>
+    <p className="message-p message-header">Username:</p>
     <p className="message-p message-content">Message Content</p>
   </div>
   );
@@ -28,9 +29,18 @@ function App()
           <Message />
           <Message />
           <Message />
+          <Message />
+          <Message />
+          <Message />
+          <Message />
+          <Message />
         </div>
         <div className="input-panel">
-
+          <form action="api/msg" method="post">
+            <input type="text" name="messageInput" id="messageInput" />
+            <button type="submit" value="" id="submit"/>
+          </form>
+          <img src={sendimg} alt="yo wtf" />
         </div>
       </div>
   )
