18c63c9428ac3d95ca2e9a760d444626742b0636
Moon-794 <21002961@hope.ac.uk>
2023-09-06 19:36:26 +0100

    Finished all Major functionality, portfolio ready

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,22 +1,44 @@
+import React, { useState, useCallback, useEffect } from "react";
 import "./App.css";
+import $ from 'jquery';
+import io from "socket.io-client";
 
-function Message()
+const socket = io('http://localhost:3000');
+
+function handleClick (e: React.ChangeEvent<any>)
+{
+    e.preventDefault();
+
+    var message = 
+    {
+      username: "username",
+      msg: $('input').val()
+    }
+
+    socket.emit('message', message);
+
+    $('input').val("");
+    console.log("Form Submitted");
+}
+
+function Message(props: React.PropsWithRef<any>)
 {
   return (
   <div className="message">
-    <p className="message-p message-header">Username:</p>
-    <p className="message-p message-content">Message Content</p>
+    <p className="message-p message-header">{props.username}:</p>
+    <p className="message-p message-content">{props.message}</p>
   </div>
   );
 }
 
 function App() 
 {
-  let messageList = [];
-  for(var i = 0; i < 15; i++)
+  const [messages, setMessages] = useState([]);
+  
+  socket.on('return', (message) => 
   {
-    messageList.push(<Message/>);
-  }
+    setMessages([<Message username={message.username} message={message.msg}/>, ...messages]);
+  });
 
   return (
       <div className="main-container">
@@ -24,17 +46,16 @@ function App()
           <h1>Full Stack Chatter</h1>
         </div>
         <div className="message-list">
-          {messageList}
+          {messages}
         </div>
         <div className="input-panel">
-          <form action="api/msg" method="post">
-            <input type="text" name="messageInput" id="messageInput" />
+          <form action="api/msg" onSubmit={handleClick} method="post">
+            <input autoComplete="off" type="text" name="messageInput" id="messageInput" />
             <button type="submit" value="" id="submit"/>
           </form>
         </div>
       </div>
   )
 }
-
 
 export default App
--- Server/server.js
+++ Server/server.js
@@ -1,13 +1,56 @@
 const express = require("express");
 const http = require("http");
+const cors = require('cors');
+const socketIO = require('socket.io');
+const {v4: uuidv4} = require('uuid');
+
+const connections = new Map();
 
 const app = express();
 const server = http.Server(app);
 const port = process.env.PORT || 3000;
+const io = socketIO(server, 
+    {
+        cors:
+        {
+            origin: "http://localhost:5173"
+        }
+    });
 
-app.get("/", (req, res) => 
+io.on('connection', (socket) => 
+{
+    console.log('A user connected');
+
+    const connectionID = uuidv4();
+    connections.set(connectionID, socket);
+
+    socket.on('message', (message) => 
+    {
+        console.log(`Received message: ${message.msg} from ${connectionID}`);
+        message.username = connectionID.substring(0, 6);
+        io.emit('return', message);
+    });
+
+    socket.on('disconnect', () => 
+    {
+        console.log('A user disconnected');
+        connections.delete(connectionID);
+    });
+})
+
+const corsOptions =
+{
+    origin: 'http://localhost:5173',
+    methods: 'POST',
+};
+
+app.use(express.json());
+app.use(cors(corsOptions));
+
+app.post("/", (req, res) => 
 {
     console.log("Request recieved.");
+    console.log(req.body);
     res.send();
 });
 
